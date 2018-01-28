01ccee0a83fac63999fbb111e4bec713f2c75a24
João Pedro <joopeeds@gmail.com>
2018-01-29 03:21:55 +1300

    New spotify style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
 
@@ -16,7 +17,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.http.get('https://joopeedsblog.appspot.com/api/exposed/recently_played')
     .subscribe(data => {
-      this.lastmusic = data[0];
+      this.lastmusic = '<a class="music" href="' + data[0].link + '"> <i class="fa fa-spotify"></i> ' + data[0].name + ' - ' + data[0].artists + '</a>'
     }, error => {
       this.lastmusic = '< <i class="fa fa-exclamation-circle"/></i>  =( >';
     });
